1ba814f910359945d90e8d62f116a8a21cf57aaf
HRodenhizer <heidi.rodenhizer@yahoo.com>
2024-04-16 12:44:37 -0700

    cleans up unnecessary files that were inadvertently committed with the v.0.0.20-alpha dataset

--- Tutorial/RemoveNARow_mainDataset.R
+++
@@ -1,4 +0,0 @@
-main = st_read("C:/Users/jdean/Documents/GitHub/ARTS/ARTS_main_dataset/v.2.0.0/ARTS_main_dataset_firstrowNA.geojson")
-main = slice(main, 2:n())
-
-st_write(main, "C:/Users/jdean/Documents/GitHub/ARTS/ARTS_main_dataset/v.2.0.0/ARTS_main_dataset.geojson", append=FALSE)
